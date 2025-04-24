869b0acda80e8897b6fc2f86d664598ee02b078b
akshit <akshit10.anand2004@gmail.com>
2025-04-24 21:23:32 +0530

    added dcoker compase and nginx for reverse proxy and load balancing

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const errorHandler = require('./middleware/errorHandler');
+const os = require('os');
 
 // Import routes
 const authRoutes = require('./routes/authRoutes');
@@ -17,12 +18,19 @@ const reportRoutes = require('./routes/reportRoutes');
 // Initialize express app
 const app = express();
 const PORT = process.env.PORT || 5000;
+const SERVER_ID = process.env.SERVER_ID || os.hostname();
 
 // Middleware
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+
+// Add server identification middleware
+app.use((req, res, next) => {
+  res.setHeader('X-Server-ID', SERVER_ID);
+  next();
+});
 
 // Routes
 app.use('/api/auth', authRoutes);
@@ -37,13 +45,26 @@ app.use('/api/reports', reportRoutes);
 app.use(errorHandler);
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the E-commerce API');
-}
-);
+  res.send(`Welcome to the E-commerce API (Server: ${SERVER_ID})`);
+});
+
+// Add an endpoint specifically for checking which server responded
+app.get('/server-info', (req, res) => {
+  res.json({
+    serverId: SERVER_ID,
+    hostname: os.hostname(),
+    platform: os.platform(),
+    uptime: os.uptime(),
+    memory: {
+      total: os.totalmem(),
+      free: os.freemem()
+    }
+  });
+});
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server ${SERVER_ID} running on port ${PORT}`);
 });
 
 module.exports = app;
